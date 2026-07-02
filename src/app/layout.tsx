5030e5969e7f3cb2d7b56d768c9cbd2b8a2a09c7
ebaymehmetaliskr-bit <ebaymehmetaliskr@gmail.com>
2026-07-02 11:05:17 +0300

    layout alias ve header guncellemesi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "../index.css";
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Geziyorum Türkiye | Akıllı Seyahat Rehberi",
